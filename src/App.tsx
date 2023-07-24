3b9d4a7033dc1e05c96533be8a479e378be9274f
Zaf <109876909+ZafirProjects@users.noreply.github.com>
2023-07-24 02:19:48 +0100

    added functionality by creating a handle submit - created an interface for the todos, ty Murad

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
+import { FormEvent, useState } from "react";
 import {} from "./styles.css";
 
+interface Itodos{
+  id: string,
+  title: string,
+  completed: boolean
+}
+
 export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState<Itodos[]>([]);
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
+  }
+
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input />
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
         </div>
-        <button className="btn">Add</button>
+        <button className="btn" type="submit">Add</button>
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
+        {todos.map((todo)=>{
+          return(<div>
+            {todo.title}
+          </div>)
+        })}
         <li>
           <label>
             <input type="checkbox"></input>
